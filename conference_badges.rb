74e128fd3fd78d12f065367107f52524e46738c8
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 01:48:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,10 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(attendees)
-  
+
   u = []
   names.each do |n|
-    u.push("Hello, #{attendees[0]}! You'll be assigned to room 1!")
+    u.push("Hello, #{n}! You'll be assigned to room 1!")
   end
   u
 
